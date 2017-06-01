e4cda74244e756f4b8e11ba47a8928e0d9c0508c
Christoph Diehl <cdiehl@mozilla.com>
2017-06-02 00:30:51 +0200

    Initial setup of FuzzOS

---
+++ receipes/fuzzmanager.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -ex
+
+cd /home/worker
+
+git clone -v --depth 1 https://github.com/mozillasecurity/fuzzmanager.git
+pip install -r fuzzmanager/requirements.txt
+python fuzzmanager/setup.py install
+
+cat > $HOME/.fuzzmanagerconf << EOL
+[Main]
+serverhost = fuzzmanager.fuzzing.mozilla.org
+serverport = 443
+serverproto = https
+serverauthtoken = 10bbb62108fcb0411ed0387420e3d7097c8d8045
+sigdir = /home/worker/signatures
+EOL
+echo "clientid =" `hostname` >> $HOME/.fuzzmanagerconf
