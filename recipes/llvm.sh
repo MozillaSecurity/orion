353ea4864f1442e4be89104cfaa93bf7128af2a7
Christoph Diehl <cdiehl@mozilla.com>
2017-07-19 18:14:25 +0200

    Add Clang 5.0

--- recipes/llvm.sh
+++ recipes/llvm.sh
@@ -3,14 +3,19 @@
 #### LLVM
 
 curl http://apt.llvm.org/llvm-snapshot.gpg.key | apt-key add -
+apt-add-repository "deb http://apt.llvm.org/zesty/ llvm-toolchain-zesty main"
 apt-add-repository "deb http://apt.llvm.org/zesty/ llvm-toolchain-zesty-4.0 main"
 
 apt-get update
 
 apt-get install -y -q --no-install-recommends --no-install-suggests \
   clang-4.0 \
+  lld-4.0 \
   lldb-4.0 \
-  lld-4.0
+  clang-5.0 \
+  lld-5.0
+# lldb-5.0
+
 
 # update-alternatives --config llvm-config
 
@@ -18,5 +23,12 @@ update-alternatives --install \
   /usr/bin/llvm-config              llvm-config      /usr/bin/llvm-config-4.0     100 \
   --slave /usr/bin/clang            clang            /usr/bin/clang-4.0               \
   --slave /usr/bin/clang++          clang++          /usr/bin/clang++-4.0             \
-  --slave /usr/bin/lldb             lldb             /usr/bin/lldb-4.0                \
-  --slave /usr/bin/llvm-symbolizer  llvm-symbolizer  /usr/bin/llvm-symbolizer-4.0
+  --slave /usr/bin/llvm-symbolizer  llvm-symbolizer  /usr/bin/llvm-symbolizer-4.0     \
+  --slave /usr/bin/lldb             lldb             /usr/bin/lldb-4.0
+
+update-alternatives --install \
+  /usr/bin/llvm-config              llvm-config      /usr/bin/llvm-config-5.0     100 \
+  --slave /usr/bin/clang            clang            /usr/bin/clang-5.0               \
+  --slave /usr/bin/clang++          clang++          /usr/bin/clang++-5.0             \
+  --slave /usr/bin/llvm-symbolizer  llvm-symbolizer  /usr/bin/llvm-symbolizer-5.0     \
+# --slave /usr/bin/lldb             lldb             /usr/bin/lldb-5.0
