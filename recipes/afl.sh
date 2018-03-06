4fe7f6016f7b78fb711d43a5f713eec13ad2a08e
Christoph Diehl <1614333+posidron@users.noreply.github.com>
2018-03-06 21:49:53 +0100

    Update recipe for AFL

--- recipes/afl.sh
+++ recipes/afl.sh
@@ -7,9 +7,8 @@ cd $HOME
 git clone -v --depth 1 https://github.com/choller/afl.git
 cd afl
 make
-cd llvm_mode
-make
-cd ..
+# FIXME: https://groups.google.com/forum/#!topic/afl-users/TDLrTu3V_Pw
+make -C llvm_mode LLVM_CONFIG=llvm-config-5.0 CC=clang-5.0
 make install
 cd ..
 rm -rf afl
