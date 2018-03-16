74386a51852cafce942be2a8532e0fb9791b6b9d
Christoph Diehl <1614333+posidron@users.noreply.github.com>
2018-03-16 23:49:42 +0100

    Update setup.sh for LibFuzzer (#6)

--- images/libfuzzer/setup.sh
+++ images/libfuzzer/setup.sh
@@ -1,72 +1,79 @@
 #!/bin/bash -ex
 cd "$HOME"
 
-# Get fuzzmanager configuration from credstash
+# Get FuzzManager configuration from credstash
 credstash get fuzzmanagerconf > .fuzzmanagerconf
 
-# Update fuzzmanager config for this instance
+# Update FuzzManager config for this instance
 mkdir -p signatures
 cat >> .fuzzmanagerconf << EOF
 sigdir = $HOME/signatures
 EOF
 
-# FuzzFetch
-fuzzfetch -o "$HOME" -n firefox -a --fuzzing --tests gtest
-
 # Firefox with ASan-/Coverage/LibFuzzer
-cd firefox
+fuzzfetch -o "$HOME" -n firefox -a --fuzzing --tests gtest
 
 # FuzzData
 FUZZDATA_URL="https://github.com/mozillasecurity/fuzzdata.git/trunk"
 
-# Corpora
-if [ -z "${CORPORA}" ]
+# LibFuzzer Corpora
+if [ -n "${CORPORA}" ]
 then
-  svn export --force "${FUZZDATA_URL}/${CORPORA}" ../corpora/
-  CORPORA="../corpora/"
+  svn export --force "${FUZZDATA_URL}/${CORPORA}" ./corpora/
+  CORPORA="./corpora/"
 fi
 
-# Tokens
-if [ -z "${TOKENS}" ]
+# LibFuzzer Dictionary Tokens
+if [ -n "${TOKENS}" ]
 then
-  svn export --force "${FUZZDATA_URL}/${TOKENS}" ../tokens.dict
-  TOKENS="-dict=../tokens.dict"
+  svn export --force "${FUZZDATA_URL}/${TOKENS}" ./tokens.dict
+  TOKENS="-dict=./tokens.dict"
 fi
 
-# ASan/LibFuzzer
-export ASAN_OPTIONS=\
+# Setup ASan
+ASAN_OPTIONS=\
 print_scariness=true:\
 strip_path_prefix=/home/worker/workspace/build/src/:\
 dedup_token_length=1:\
 print_cmdline=true:\
 detect_stack_use_after_scope=true:\
-detect_invalid_pointer_pairs=1:\
+detect_invalid_pointer_pairs=2:\
 strict_init_order=true:\
 check_initialization_order=true:\
 allocator_may_return_null=true:\
+start_deactivated=true:\
 ${ASAN}
-export LIBFUZZER="${LIBFUZZER:-SdpParser}"
-export LIBFUZZER_ARGS=("-print_pcs=1" "${TOKENS}" "${LIBFUZZER_ARGS}")
-export MOZ_RUN_GTEST=1
 
-xvfb-run -s '-screen 0 1024x768x24' \
-    ../fuzzmanager/misc/libfuzzer/libfuzzer.py \
-        --sigdir ../signatures \
-        --tool "LibFuzzer-${LIBFUZZER}" \
-        --env "${ASAN_OPTIONS//:/ }" \
-        --cmd ./firefox "${LIBFUZZER_ARGS[@]}" "${CORPORA}"
+# Run reporter for EC2
+tee run-ec2report.sh << EOF
+#!/bin/bash
+./fuzzmanager/EC2Reporter/EC2Reporter.py --report-from-file stats --keep-reporting 60 --random-offset 30
+EOF
+chmod u+x run-ec2report.sh
+#screen -t ec2report -dmS ec2report ./run-ec2report.sh
+
+# Setup LibFuzzer
+export FUZZER="${FUZZER:-SdpParser}"
+export LIBFUZZER=1
+export MOZ_RUN_GTEST=1
+LIBFUZZER_ARGS=("-print_pcs=1" "-handle_segv=0" "-handle_bus=0" "-handle_abrt=0" ${LIBFUZZER_ARGS} ${TOKENS} ${CORPORA})
+
+# Run LibFuzzer
+./fuzzmanager/misc/afl-libfuzzer/afl-libfuzzer-daemon.py \
+  --fuzzmanager \
+  --libfuzzer \
+  --sigdir ./signatures \
+  --tool "LibFuzzer-${FUZZER}" \
+  --env "ASAN_OPTIONS=${ASAN_OPTIONS//:/ }" \
+  --cmd ./firefox/firefox "${LIBFUZZER_ARGS[@]}"
 
 # Minimize Crash
 #   xvfb-run -s '-screen 0 1024x768x24' ./firefox -minimize_crash=1 -max_total_time=60 crash-<hash>
-
+#
 # ASan Options
-#   start_deactivated=true
-#   handle_ioctl=true
 #   detect_deadlocks=true
+#   handle_ioctl=true
 #   intercept_tls_get_addr=true
 #   leak_check_at_exit=true # LSAN not enabled in --enable-fuzzing
 #   strict_string_checks=false # GLib textdomain() error
 #   detect_stack_use_after_return=false # nsXPConnect::InitStatics() error
-
-# LibFuzzer Options
-# -jobs=4 # not supported yet by libfuzzer.py
