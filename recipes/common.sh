6e59c85ce9696c521d967213405bd971f41adc69
posidron <1614333+posidron@users.noreply.github.com>
2018-06-27 19:55:07 +0200

    Add retry() Bash function

--- recipes/common.sh
+++ recipes/common.sh
@@ -1,8 +1,17 @@
 #!/bin/bash -ex
 
-# Calls `apt-get install` on it's arguments but marks them as automatically installed
-# previously installed packages are not affected.
-apt-install-auto () {
+function retry {
+  # shellcheck disable=SC2015
+  for _ in {1..9}
+  do
+    "$@" && return || sleep 30
+  done
+  "$@"
+}
+
+# Calls `apt-get install` on it's arguments but marks them as automatically installed.
+# Previously installed packages are not affected.
+function apt-install-auto () {
   new=()
   for pkg in "$@"; do
     if ! dpkg -l "$pkg" 2>&1 | grep -q ^ii; then
