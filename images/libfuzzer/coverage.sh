519cb04eba65c6f8a32670705a8147d3a031d737
posidron <1614333+posidron@users.noreply.github.com>
2018-06-28 17:32:54 +0200

    Add support for LibFuzzer coverage

---
+++ images/libfuzzer/coverage.sh
@@ -0,0 +1,68 @@
+#!/bin/bash -xu
+# shellcheck disable=SC2016
+
+# %<---[setup]----------------------------------------------------------------
+
+WORKDIR=${WORKDIR:-$HOME}
+cd "$WORKDIR" || exit
+
+# Fetch a Firefox coverage build and its coverage notes files plus gtest.
+# We might have a volume attached which mounts a build into the container.
+if [[ ! -d "$WORKDIR/firefox" ]]
+then
+    #python -m fuzzfetch --coverage --tests gtest -n firefox -o "$WORKDIR"
+    python -m fuzzfetch \
+        --build gecko.v2.mozilla-central.latest.firefox.linux64-fuzzing-asan-opt-cov \
+        --tests gtest \
+        -n firefox \
+        -o "$WORKDIR"
+    chmod -R 755 firefox
+    (cd firefox \
+    && curl -LO https://index.taskcluster.net/v1/task/gecko.v2.mozilla-central.latest.firefox.linux64-fuzzing-asan-opt-cov/artifacts/public/build/target.code-coverage-gcno.zip \
+    && unzip target.code-coverage-gcno.zip)
+fi
+
+# Download mozilla-central source code.
+# We might have a volume attached which mounts the source into the container.
+REVISION=$(rg -Nor '$1' "SourceStamp=(.*)" "$WORKDIR/firefox/platform.ini")
+if [[ ! -d "$WORKDIR/mozilla-central" ]]
+then
+    export REVISION
+    hg clone -r "$REVISION" https://hg.mozilla.org/mozilla-central
+else
+    (cd mozilla-central && hg update -r "$REVISION")
+fi
+
+# Setup required coverage environment variables.
+export COVERAGE=1
+export GCOV_PREFIX_STRIP=6
+export GCOV_PREFIX="$WORKDIR/firefox"
+
+# %<---[fuzzer]---------------------------------------------------------------
+
+timeout -s 10 -k $((COVRUNTIME + 30)) "$COVRUNTIME" ./libfuzzer.sh
+
+# %<---[coverage]-------------------------------------------------------------
+
+# Collect coverage count data.
+grcov "$WORKDIR/firefox" \
+    --llvm \
+    -t coveralls+ \
+    --commit-sha "$REVISION" \
+    --token NONE \
+    -s "$WORKDIR/mozilla-central" \
+    -p "$(rg -Nor '$1' "pathprefix = (.*)" "$WORKDIR/firefox/firefox.fuzzmanagerconf")" \
+    > "$WORKDIR/coverage.json"
+
+# Submit coverage data.
+python -m CovReporter.CovReporter \
+    --repository mozilla-central \
+    --description "FuzzOS-LibFuzzer ($FUZZER,rt=$COVRUNTIME)" \
+    --tool "FuzzOS-LibFuzzer-$FUZZER" \
+    --submit "$WORKDIR/coverage.json"
+
+# Disable our pool.
+if [[ $EC2SPOTMANAGER_POOLID ]]
+then
+    python -m EC2Reporter --disable "$EC2SPOTMANAGER_POOLID"
+fi
--- images/libfuzzer/setup.sh
+++ images/libfuzzer/setup.sh
@@ -1,94 +1,10 @@
 #!/bin/bash -ex
-cd "$HOME"
 
-# Get FuzzManager configuration from credstash
-credstash get fuzzmanagerconf > .fuzzmanagerconf
-
-# Update FuzzManager config for this instance
-mkdir -p signatures
-cat >> .fuzzmanagerconf << EOF
-sigdir = $HOME/signatures
-EOF
-
-# Firefox with ASan-/Coverage/LibFuzzer
-fuzzfetch -o "$HOME" -n firefox -a --fuzzing --tests gtest
-
-# FuzzData
-FUZZDATA_URL="https://github.com/mozillasecurity/fuzzdata.git/trunk"
-
-# ContentIPC
-if [ -n "$MOZ_IPC_MESSAGE_FUZZ_BLACKLIST" ]
+if [[ $COVERAGE ]]
 then
-  mkdir -p settings/ipc
-  svn export --force "$FUZZDATA_URL/$MOZ_IPC_MESSAGE_FUZZ_BLACKLIST" "$MOZ_IPC_MESSAGE_FUZZ_BLACKLIST"
-  export MOZ_IPC_MESSAGE_FUZZ_BLACKLIST="$HOME/$MOZ_IPC_MESSAGE_FUZZ_BLACKLIST"
-fi
-
-# LibFuzzer Corpora
-if [ -n "$CORPORA" ]
-then
-  svn export --force "$FUZZDATA_URL/$CORPORA" ./corpora/
-  CORPORA="./corpora/"
+    echo "Launching coverage LibFuzzer run."
+    ./coverage.sh
 else
-  mkdir ./corpora
-  CORPORA="./corpora/"
+    echo "Launching LibFuzzer run."
+    ./libfuzzer.sh
 fi
-
-
-# LibFuzzer Dictionary Tokens
-if [ -n "$TOKENS" ]
-then
-  svn export --force "$FUZZDATA_URL/$TOKENS" ./tokens.dict
-  TOKENS="-dict=./tokens.dict"
-fi
-
-# Setup ASan
-export ASAN_SYMBOLIZER_PATH=/usr/bin/llvm-symbolizer
-ASAN_OPTIONS=\
-print_scariness=true:\
-strip_path_prefix=/builds/worker/workspace/build/src/:\
-dedup_token_length=1:\
-print_cmdline=true:\
-detect_stack_use_after_scope=true:\
-detect_invalid_pointer_pairs=2:\
-strict_init_order=true:\
-check_initialization_order=true:\
-allocator_may_return_null=true:\
-start_deactivated=false:\
-strict_string_checks=true:\
-$ASAN
-
-# Run reporter for EC2
-tee run-ec2report.sh << EOF
-#!/bin/bash
-./fuzzmanager/EC2Reporter/EC2Reporter.py --report-from-file stats --keep-reporting 60 --random-offset 30
-EOF
-chmod u+x run-ec2report.sh
-#screen -t ec2report -dmS ec2report ./run-ec2report.sh
-
-# Setup LibFuzzer
-export FUZZER="${FUZZER:-SdpParser}"
-export LIBFUZZER=1
-export MOZ_RUN_GTEST=1
-# shellcheck disable=SC2206
-LIBFUZZER_ARGS=("-print_pcs=1" "-handle_segv=0" "-handle_bus=0" "-handle_abrt=0" $LIBFUZZER_ARGS $TOKEN $CORPORA)
-
-# Run LibFuzzer
-./fuzzmanager/misc/afl-libfuzzer/afl-libfuzzer-daemon.py \
-  --fuzzmanager \
-  --libfuzzer \
-  --sigdir ./signatures \
-  --tool "LibFuzzer-$FUZZER" \
-  --env "ASAN_OPTIONS=${ASAN_OPTIONS//:/ }" \
-  --cmd ./firefox/firefox "${LIBFUZZER_ARGS[@]}"
-
-# Minimize Crash
-#   xvfb-run -s '-screen 0 1024x768x24' ./firefox -minimize_crash=1 -max_total_time=60 crash-<hash>
-#
-# ASan Options
-#   detect_deadlocks=true
-#   handle_ioctl=true
-#   intercept_tls_get_addr=true
-#   leak_check_at_exit=true # LSAN not enabled in --enable-fuzzing
-#   strict_string_checks=false # GLib textdomain() error
-#   detect_stack_use_after_return=false # nsXPConnect::InitStatics() error
