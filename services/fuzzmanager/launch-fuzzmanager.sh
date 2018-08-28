2b4e7ecc6a89fd6431aad95acc750c8a1614bf7c
Gary Kwong <nth10sd@gmail.com>
2018-08-28 02:27:54 -0700

    Take 1 on the proposed FuzzManager docker structure.

---
+++ services/fuzzmanager/launch-fuzzmanager.sh
@@ -0,0 +1,4 @@
+#!/bin/bash -ex
+
+echo "Simulate running manage.py"
+# python manage.py runserver
---
+++ services/fuzzmanager/recipes/fuzzmanager.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -ex
+
+echo "Simulate installing FuzzManager"
+
+#### Install FuzzManager
+
+# cd /home/worker
+# git clone --depth 1 --no-tags https://github.com/mozillasecurity/fuzzmanager.git
+# pip install ./fuzzmanager
+# pip install boto
