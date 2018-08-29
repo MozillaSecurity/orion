519cc3fd2b77d87f0c1a2268bf6801bd04e91ab9
Jesse Schwartzentruber <truber@mozilla.com>
2018-08-29 14:08:11 -0400

    [grizzly-android] Don't create kvm node in the build step
    Doesn't seem to be possible with the docker version in travis ci

--- services/grizzly-android/recipes/all.sh
+++ services/grizzly-android/recipes/all.sh
@@ -11,10 +11,6 @@ apt-get autoremove -y
 rm -rf /var/lib/apt/lists/*
 rm -rf /root/.cache/*
 
-# Create the kvm node
-mknod /dev/kvm c 10 "$(grep '\<kvm\>' /proc/misc | cut -f 1 -d' ')"
-chmod 0666 /dev/kvm
-
 python /tmp/recipes/emulator.py
 
 chown -R worker:worker /home/worker
