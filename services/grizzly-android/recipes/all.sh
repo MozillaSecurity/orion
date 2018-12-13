3b298fe1b1971edbe6459dedd240337a3e973102
Tyson Smith <tyson.w.smith@gmail.com>
2018-12-13 11:58:41 -0800

    Install qemu-kvm

--- services/grizzly-android/recipes/all.sh
+++ services/grizzly-android/recipes/all.sh
@@ -1,6 +1,15 @@
 #!/bin/bash -ex
 
+apt-get update -y -qq
+apt-get install -q -y --no-install-recommends \
+    qemu-kvm
+apt-get clean -y
+apt-get autoclean -y
+apt-get autoremove -y
+rm -rf /var/lib/apt/lists/*
+
 pip install -r /tmp/recipes/requirements.txt
 python /tmp/recipes/emulator.py install avd
 
 chown -R worker:worker /home/worker
+adduser worker kvm
