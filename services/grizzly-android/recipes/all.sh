19a0617ebd9fc48f15c03c1a8d4dbec4e8e53de0
Tyson Smith <tyson.w.smith@gmail.com>
2018-12-13 17:58:32 -0800

    Create /dev/kvm if it does not exist

--- services/grizzly-android/recipes/all.sh
+++ services/grizzly-android/recipes/all.sh
@@ -13,6 +13,10 @@ python /tmp/recipes/emulator.py install avd
 
 chown -R worker:worker /home/worker
 
+if [ ! -e /dev/kvm ]
+then
+    mknod /dev/kvm c 10 "$(grep '\<kvm\>' /proc/misc | cut -f 1 -d' ')"
+fi
 usermod -a -G kvm worker
 chgrp kvm /dev/kvm
 chmod 0660 /dev/kvm
