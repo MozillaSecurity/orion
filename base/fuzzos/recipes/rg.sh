3ef940dce4b7199e3a6fad04af68e4365a57b28c
posidron <1614333+posidron@users.noreply.github.com>
2018-12-18 02:30:02 +0100

    Pin version of ripgrep to 0.10.0

--- base/fuzzos/recipes/rg.sh
+++ base/fuzzos/recipes/rg.sh
@@ -5,7 +5,7 @@ source ./recipes/common.sh
 
 #### Install rg (ripgrep)
 
-LATEST_VERSION=$(curl -s "https://github.com/BurntSushi/ripgrep/releases/latest" | grep -o 'tag/[v.0-9]*' | awk -F/ '{print $2}')
-retry curl -LO "https://github.com/BurntSushi/ripgrep/releases/download/${LATEST_VERSION}/ripgrep_${LATEST_VERSION}_amd64.deb"
-apt install ./"ripgrep_${LATEST_VERSION}_amd64.deb"
-rm "ripgrep_${LATEST_VERSION}_amd64.deb"
+STABLE_VERSION="0.10.0"
+curl -LO "https://github.com/BurntSushi/ripgrep/releases/download/${STABLE_VERSION}/ripgrep_${STABLE_VERSION}_amd64.deb"
+apt install "./ripgrep_${STABLE_VERSION}_amd64.deb"
+rm "ripgrep_${STABLE_VERSION}_amd64.deb"
