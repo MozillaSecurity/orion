7fa7adb22b9c0e54ffc00bc4bdda0d411c921711
posidron <1614333+posidron@users.noreply.github.com>
2018-12-21 00:12:21 +0100

    Add documentation to ci-docker-login.sh

--- scripts/ci-docker-login.sh
+++ scripts/ci-docker-login.sh
@@ -1,12 +1,14 @@
 #!/bin/bash -exu
 sudo apt-get install -y -qq --no-install-recommends --no-install-suggests pass
 
+# Download the latest version of `docker-credential-pass`
 LATEST_VERSION=$(curl -Ls --retry 5 "https://$GH_TOKEN@api.github.com/repos/docker/docker-credential-helpers/releases/latest" | grep -Po '"tag_name": "\K.*?(?=")')
 echo "$LATEST_VERSION"
 curl -LO "https://github.com/docker/docker-credential-helpers/releases/download/$LATEST_VERSION/docker-credential-pass-$LATEST_VERSION-amd64.tar.gz"
 tar xvf "docker-credential-pass-$LATEST_VERSION-amd64.tar.gz"
 sudo mv docker-credential-pass /usr/local/bin
 
+# Setup a dummy secret key for the `pass` credentials store initialization required by the Docker client.
 gpg2 --batch --gen-key <<-EOF
 %echo Generating a standard key
 Key-Type: DSA
@@ -23,4 +25,7 @@ EOF
 key=$(gpg2 --no-auto-check-trustdb --list-secret-keys | grep ^sec | cut -d/ -f2 | cut -d" " -f1)
 pass init "$key"
 
+# Pass the encrypted DOCKER_PASS to the `docker` client. Encrypted in the build logs.
+# Uses previously setup `credsStore` in ~/.docker/config.json as credentials store.
+# We perform a `docker logout` after the session in Travis ends.
 echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin
