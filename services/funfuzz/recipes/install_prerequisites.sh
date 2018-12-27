c17742452a3983909afcc27c8994d247888f9dcb
Gary Kwong <nth10sd@gmail.com>
2018-12-27 13:51:33 -0800

    locale-gen is already run via FuzzOS.

--- services/funfuzz/recipes/install_prerequisites.sh
+++ services/funfuzz/recipes/install_prerequisites.sh
@@ -1,8 +1,5 @@
 #!/bin/bash -ex
 
-echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
-locale-gen en_US.UTF-8
-
 apt-get update -y -qq
 # Required to use apt-key
 apt-get install -q -y --no-install-recommends --no-install-suggests dirmngr
