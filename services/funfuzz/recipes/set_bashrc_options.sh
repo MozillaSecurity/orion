ff6378171efbc01203856e0c7bc06c2cd67c24e2
Gary Kwong <nth10sd@gmail.com>
2018-12-27 04:40:06 -0800

    Fix funfuzz_command_test failure by updating the exit code and moving environment variables to the Dockerfile.

--- services/funfuzz/recipes/set_bashrc_options.sh
+++ services/funfuzz/recipes/set_bashrc_options.sh
@@ -6,18 +6,6 @@ cat << EOF >> .bashrc
 
 ulimit -c unlimited
 
-# Expand bash shell history length
-export HISTTIMEFORMAT="%h %d %H:%M:%S "
-HISTSIZE=10000
-
-# Modify bash prompt
-export PS1="[\u@\h \d \t \W ] $ "
-
-export LD_LIBRARY_PATH=.
-export ASAN_SYMBOLIZER_PATH=/usr/bin/llvm-symbolizer
-
-PATH=/home/worker/.cargo/bin:/home/worker/.local/bin:$PATH
-
 ccache -M 12G
 EOF
 
