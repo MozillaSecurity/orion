ed04080326d13917af07f3f1d26e3e7ffb0ff87d
posidron <1614333+posidron@users.noreply.github.com>
2019-04-05 21:25:40 +0200

    Add Licensing

--- monorepo.py
+++ monorepo.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 # coding: utf-8
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 """
 A script that aids in building and publishing multiple 🐳 containers as microservices within a single repository,
 aka monorepo.
--- services/grizzly-android/recipes/kvm.sh
+++ services/grizzly-android/recipes/kvm.sh
@@ -1,4 +1,8 @@
 #!/bin/bash -ex
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 if [ ! -e /dev/kvm ]
 then
     mknod /dev/kvm c 10 "$(grep '\<kvm\>' /proc/misc | cut -f 1 -d' ')"
