5d764c7276d9500b2837962fb80dd0baf81f4b6d
posidron <1614333+posidron@users.noreply.github.com>
2019-04-10 11:20:53 +0200

    Export PS1 to something more distinguishable

--- base/linux/fuzzos/recipes/fuzzos.sh
+++ base/linux/fuzzos/recipes/fuzzos.sh
@@ -37,6 +37,7 @@ mkdir ~/.bin
 cat << 'EOF' >> ~/.bashrc
 # FuzzOS
 export PATH=$HOME/.bin:$PATH
+export PS1='🐳  \[\033[1;36m\]\h \[\033[1;34m\]\W\[\033[0;35m\] \[\033[1;36m\]# \[\033[0m\]'
 EOF
 
 # Add `cleanup.sh` to let images perform standard cleanup operations.
