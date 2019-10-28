135c7885420dec7a7b8554cc5e4cf5c4b7a5748e
posidron <1614333+posidron@users.noreply.github.com>
2019-10-28 18:43:19 +0100

    Add local test build script

---
+++ scripts/test-builds.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+export DOCKER_ORG=posidron
+
+TRAVIS_PULL_REQUEST=false TRAVIS_BRANCH=master TRAVIS_EVENT_TYPE=cron ./monorepo.py -ci travis -build -test -deliver -path core/linux
+TRAVIS_PULL_REQUEST=false TRAVIS_BRANCH=master TRAVIS_EVENT_TYPE=cron ./monorepo.py -ci travis -build -test -deliver -path base/linux/fuzzos
+TRAVIS_PULL_REQUEST=false TRAVIS_BRANCH=master TRAVIS_EVENT_TYPE=cron ./monorepo.py -ci travis -build -test -deliver -path services/credstash
+
+TRAVIS_PULL_REQUEST=false TRAVIS_BRANCH=master TRAVIS_EVENT_TYPE=cron ./monorepo.py -ci travis -build -test -deliver -path services/funfuzz
+TRAVIS_PULL_REQUEST=false TRAVIS_BRANCH=master TRAVIS_EVENT_TYPE=cron ./monorepo.py -ci travis -build -test -deliver -path services/grizzly
+TRAVIS_PULL_REQUEST=false TRAVIS_BRANCH=master TRAVIS_EVENT_TYPE=cron ./monorepo.py -ci travis -build -test -deliver -path services/grizzly-android
+TRAVIS_PULL_REQUEST=false TRAVIS_BRANCH=master TRAVIS_EVENT_TYPE=cron ./monorepo.py -ci travis -build -test -deliver -path services/fuzzmanager
+TRAVIS_PULL_REQUEST=false TRAVIS_BRANCH=master TRAVIS_EVENT_TYPE=cron ./monorepo.py -ci travis -build -test -deliver -path services/linter
+TRAVIS_PULL_REQUEST=false TRAVIS_BRANCH=master TRAVIS_EVENT_TYPE=cron ./monorepo.py -ci travis -build -test -deliver -path services/libfuzzer
+TRAVIS_PULL_REQUEST=false TRAVIS_BRANCH=master TRAVIS_EVENT_TYPE=cron ./monorepo.py -ci travis -build -test -deliver -path services/u2f-hid-rs
