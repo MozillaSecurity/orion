b7d6647330555f9da0e9d25d517931ef6adc130c
Jesse Schwartzentruber <truber@mozilla.com>
2020-02-11 08:34:41 -0500

    Fix radamsa recipe

--- base/linux/fuzzos/recipes/radamsa.sh
+++ base/linux/fuzzos/recipes/radamsa.sh
@@ -1,14 +1,24 @@
-#!/bin/bash -ex
+#!/usr/bin/env bash
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+set -e
+set -x
+
+# shellcheck source=base/linux/fuzzos/recipes/common.sh
+source "${0%/*}/common.sh"
+
+# Wget is used in the make process
+apt-install-auto wget
 
 # Build radamsa
 TMPD="$(mktemp -d -p. radamsa.build.XXXXXXXXXX)"
 ( cd "$TMPD"
   git clone --depth 1 --no-tags https://gitlab.com/akihe/radamsa.git
   ( cd radamsa
-    CC=clang make
+    export CC=clang
+    make
     make install
   )
 )
