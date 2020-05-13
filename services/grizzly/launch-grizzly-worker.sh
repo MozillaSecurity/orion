d00f12ae0867c29b7450e420fadcaadec1388826
pyoor <pyoor@users.noreply.github.com>
2020-05-13 14:20:48 -0400

    Use standard gcov-7 for grcov

--- services/grizzly/launch-grizzly-worker.sh
+++ services/grizzly/launch-grizzly-worker.sh
@@ -68,6 +68,8 @@ pip3 install --user -U -e ./bearspray
 
 update_ec2_status "Setup: launching bearspray"
 
+export GCOV=/usr/local/bin/gcov-7
+
 screen -dmLS grizzly /bin/bash
 sleep 5
 screen -S grizzly -X screen rwait run "$wait_token" python3 -m bearspray --screen --xvfb
