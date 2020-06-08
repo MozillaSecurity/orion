e061b851848424e39c23f20e9f4de1c85f274367
Jesse Schwartzentruber <truber@mozilla.com>
2020-06-08 16:21:40 -0400

    Add to .travis.yml, use taskcluster cli, and base on ci-node-12.

--- services/domino-web-tests/launch.sh
+++ services/domino-web-tests/launch.sh
@@ -1,17 +1,31 @@
-#!/bin/bash -ex
+#!/bin/bash
 
-cd /root
-set +x
-curl -sSL http://taskcluster/secrets/v1/secret/project/fuzzing/deploy-domino-web-tests | jshon -e secret -e key -u >.ssh/id_ecdsa.domino_web_tests
-curl -sSL http://taskcluster/secrets/v1/secret/project/fuzzing/deploy-domino | jshon -e secret -e key -u >.ssh/id_rsa.domino
-curl -sSL http://taskcluster/secrets/v1/secret/project/fuzzing/deploy-gridl | jshon -e secret -e key -u >.ssh/id_rsa.gridl
-curl -sSL http://taskcluster/secrets/v1/secret/project/fuzzing/deploy-octo-private | jshon -e secret -e key -u >.ssh/id_rsa.octo
+set -e
 set -x
-chmod 0400 .ssh/id_ecdsa.domino_web_tests .ssh/id_rsa.domino .ssh/id_rsa.gridl .ssh/id_rsa.octo
+set -o pipefail
 
-git clone git@domino-web-tests:MozillaSecurity/domino-web-tests
+function retry () {
+  for _ in {1..9}; do
+    "$@" && return
+    sleep 30
+  done
+  "$@"
+}
+
+set +x
+retry taskcluster api secrets get project/fuzzing/deploy-domino-web-tests | jshon -e secret -e key -u >.ssh/id_ecdsa.domino_web_tests
+retry taskcluster api secrets get project/fuzzing/deploy-domino | jshon -e secret -e key -u >.ssh/id_rsa.domino
+retry taskcluster api secrets get project/fuzzing/deploy-gridl | jshon -e secret -e key -u >.ssh/id_rsa.gridl
+retry taskcluster api secrets get project/fuzzing/deploy-octo-private | jshon -e secret -e key -u >.ssh/id_rsa.octo
+set -x
+chmod 0400 .ssh/id_*
+
+git init domino-web-tests
 cd domino-web-tests
-npm install
-npm update domino gridl
+git remote add origin git@domino-web-tests:MozillaSecurity/domino-web-tests
+retry git fetch -q --depth=5 origin master
+git -c advice.detachedHead=false checkout origin/master
+retry npm install
+retry npm update domino gridl
 git commit -a -m "Update package-lock.json"
-git push
+retry git push origin master
