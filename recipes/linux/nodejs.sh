5b11b2ef87f113bb16ccbdd2809d4202ed2052bb
pyoor <pyoor@users.noreply.github.com>
2020-06-09 12:36:54 -0400

    Upgrade node to v12.x

--- recipes/linux/nodejs.sh
+++ recipes/linux/nodejs.sh
@@ -17,6 +17,6 @@ apt-install-auto \
     ca-certificates \
     curl
 
-curl --retry 5 -sL https://deb.nodesource.com/setup_10.x | bash -
+curl --retry 5 -sL https://deb.nodesource.com/setup_12.x | bash -
 sys-embed nodejs
 strip --strip-unneeded /usr/bin/node
