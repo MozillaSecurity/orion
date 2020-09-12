f2908e3f5bc781f1c4b0aec2680acdd48edccc63
pyoor <pyoor@users.noreply.github.com>
2020-09-12 09:25:35 -0400

    [bugmon] Simplify bugmon by inheriting from grizzly image

--- services/bugmon/setup.sh
+++ services/bugmon/setup.sh
@@ -7,92 +7,12 @@ set -e
 set -x
 set -o pipefail
 
-# shellcheck source=recipes/linux/common.sh
-source "${0%/*}/common.sh"
-
-#### Bootstrap Packages
-
-sys-update
-
-#### Install recipes
-
+# Install taskcluster CLI
 cd "${0%/*}"
 ./taskcluster.sh
 
-#### Install packages
-
-# shellcheck source=recipes/linux/dbgsyms.sh
-source ./dbgsyms.sh
-
-# packages without recommends (or *wanted* recommends)
-# TODO: we should expand recommends and just have one list
-packages=(
-  libasound2
-  libc6-dbg
-  libdbus-glib-1-2
-  libglu1-mesa
-  libosmesa6
-  libpulse0
-  p7zip-full
-  python3-wheel
-  screen
-  subversion
-  ubuntu-desktop-minimal
-  ubuntu-restricted-addons
-  wget
-  zip
-)
-
-# packages with *unwanted* recommends
-packages_with_recommends=(
-  apt-utils
-  build-essential
-  bzip2
-  curl
-  dbus
-  gdb
-  git
-  gpg-agent
-  jshon
-  less
-  libavcodec-extra
-  libgtk-3-0
-  locales
-  nano
-  openssh-client
-  python3
-  python3-dev
-  python3-pip
-  python3-setuptools
-  python3-venv
-  python-is-python3
-  software-properties-common
-  unzip
-  valgrind
-  xvfb
-)
-
-dbgsym_packages=(
-  libcairo2
-  libegl1
-  libgl1
-  libglvnd0
-  libglx0
-  libgtk-3-0
-  libwayland-egl1
-)
-
-sys-embed "${packages_with_recommends[@]}"
-retry apt-get install -y -qq "${packages[@]}"
-
-# We want full symbols for things GTK/Mesa related where we find crashes.
-sys-embed-dbgsym "${dbgsym_packages[@]}"
-
-retry pip3 install \
-  psutil \
-  virtualenv \
-  git+https://github.com/cgoldberg/xvfbwrapper.git
-
+# shellcheck source=recipes/linux/common.sh
+source "${0%/*}/common.sh"
 
 git init bugmon-tc
 (
@@ -103,9 +23,8 @@ git init bugmon-tc
   pip3 install .
 )
 
-#### Clean up
-
-./cleanup.sh
+# Cleanup grizzly scripts
+rm /home/worker/launch-grizzly*
 
 #### Create aritfact directory
 mkdir /bugmon-artifacts
