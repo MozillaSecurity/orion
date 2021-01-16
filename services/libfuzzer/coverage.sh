b15cb4d48f16cecd64246082fbd9365643851fa4
Jesse Schwartzentruber <truber@mozilla.com>
2021-01-15 23:42:46 -0500

    [libfuzzer] Switch coverage revision source to TC

--- services/libfuzzer/coverage.sh
+++ services/libfuzzer/coverage.sh
@@ -18,7 +18,7 @@ source ~/.local/bin/common.sh
 # Setup required coverage environment variables.
 export COVERAGE=1
 
-REVISION="$(curl --retry 5 -sL https://build.fuzzing.mozilla.org/builds/coverage-revision.txt)"
+REVISION="$(curl --retry 5 -sL https://community-tc.services.mozilla.com/api/index/v1/task/project.fuzzing.coverage-revision.latest/artifacts/public/coverage-revision.txt)"
 export REVISION
 
 # Our default target is Firefox, but we support targetting the JS engine instead.
