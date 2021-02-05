570e9d7c6d94276df844e41759e9276d192c2fe0
pyoor <pyoor@users.noreply.github.com>
2021-02-05 10:29:28 -0500

    Install bugmon_tc via pip rather than clone

--- services/bugmon/launch.sh
+++ services/bugmon/launch.sh
@@ -15,18 +15,11 @@ function tc-get-secret () {
   TASKCLUSTER_ROOT_URL="${TASKCLUSTER_PROXY_URL-$TASKCLUSTER_ROOT_URL}" retry taskcluster api secrets get "project/fuzzing/$1"
 }
 
+export PATH=$PATH:/home/worker/.local/bin
 export ARTIFACT_DEST="/bugmon-artifacts"
 export TC_ARTIFACT_ROOT="project/fuzzing/bugmon"
 
-git init bugmon-tc
-(
-  cd bugmon-tc
-  git remote add -t master origin https://github.com/MozillaSecurity/bugmon-tc.git
-  retry git fetch -v --depth 1 --no-tags origin master
-  git reset --hard FETCH_HEAD
-  pip3 install .
-)
-
+pip3 install git+https://github.com/MozillaSecurity/bugmon-tc.git
 case "$BUG_ACTION" in
   monitor | report)
     BZ_API_KEY="$(tc-get-secret bz-api-key | jshon -e secret -e key -u)"
