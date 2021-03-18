32e29352f0531a2d1a530815c9f761d7d5abccc4
Jesse Schwartzentruber <truber@mozilla.com>
2021-03-18 09:31:23 -0400

    [recipes] Fix recipe test failures
    - orion_decision needs SRCDIR set for the test
    - worker needs locales installed from apt

--- recipes/linux/worker.sh
+++ recipes/linux/worker.sh
@@ -11,16 +11,34 @@ set -o pipefail
 # shellcheck source=recipes/linux/common.sh
 source "${0%/*}/common.sh"
 
-# Create worker user and generate locale
-LANGUAGE="${LANGUAGE-en}"
-LANG="${LANG-en_US.UTF-8}"
+case "${1-install}" in
+  install)
+    sys-embed locales
 
-useradd -d /home/worker -s /bin/bash -m worker
-cat << EOF >> /etc/environment
-LANGUAGE=${LANGUAGE}
-LANG=${LANG}
-LC_ALL=${LANG}
-EOF
-sed -i "s/# ${LANG}/${LANG}/" /etc/locale.gen
-echo "LANG=${LANG}" > /etc/locale.conf
-locale-gen "$LANG"
+    # Create worker user and generate locale
+    LANGUAGE="${LANGUAGE-en}"
+    LANG="${LANG-en_US.UTF-8}"
+
+    useradd -d /home/worker -s /bin/bash -m worker
+    cat <<- EOF >> /etc/environment
+	LANGUAGE=${LANGUAGE}
+	LANG=${LANG}
+	LC_ALL=${LANG}
+	EOF
+
+    sed -i "s/# ${LANG}/${LANG}/" /etc/locale.gen
+    echo "LANG=${LANG}" > /etc/locale.conf
+    locale-gen "$LANG"
+    ;;
+  test)
+    su - worker -c "echo 'hello world'"
+    # shellcheck disable=SC2016
+    [[ "$(su - worker -c 'echo "$LANGUAGE"')" = "en" ]]
+    # shellcheck disable=SC2016
+    [[ "$(su - worker -c 'echo "$LANG"')" = "en_US.UTF-8" ]]
+    # shellcheck disable=SC2016
+    [[ "$(su - root -c 'echo "$LANGUAGE"')" = "en" ]]
+    # shellcheck disable=SC2016
+    [[ "$(su - root -c 'echo "$LANG"')" = "en_US.UTF-8" ]]
+    ;;
+esac
