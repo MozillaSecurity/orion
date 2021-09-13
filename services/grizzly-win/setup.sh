c5162380d6ca2b9c9835fa2b59024ef4e8e6a2c7
pyoor <pyoor@users.noreply.github.com>
2021-09-13 10:09:29 -0400

    [grizzly-win] Update npm v7.22.0

--- services/grizzly-win/setup.sh
+++ services/grizzly-win/setup.sh
@@ -81,6 +81,7 @@ mv "node-v${VER}-win-x64" msys64/opt/node
 PATH="$PWD/msys64/opt/node:$PATH"
 which node
 node -v
+curl -qL https://www.npmjs.com/install.sh | npm_install="7.22.0" sh
 npm -v
 
 # install utils to match linux ci images
