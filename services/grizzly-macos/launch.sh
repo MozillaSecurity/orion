d9fcf6db6ffd5f913f6885fef06c60a3ffe6f1dc
Jesse Schwartzentruber <truber@mozilla.com>
2022-01-20 17:08:42 -0500

    [grizzly] Add macos base image

---
+++ services/grizzly-macos/launch.sh
@@ -0,0 +1,138 @@
+#!/bin/bash
+set -e -x
+
+trap 'jobs -p | xargs -r kill' EXIT
+
+retry () {
+  i=0
+  while [ $i -lt 9 ]
+  do
+    "$@" && return
+    sleep 30
+    i="${i+1}"
+  done
+  "$@"
+}
+
+status () {
+  if [ -n "$TASKCLUSTER_FUZZING_POOL" ]
+  then
+    python -m TaskStatusReporter --report "$@" || true
+  fi
+}
+
+set +x
+curl --retry 5 -L "$TASKCLUSTER_PROXY_URL/secrets/v1/secret/project/fuzzing/google-logging-creds" | python -c "import json,sys;json.dump(json.load(sys.stdin)['secret']['key'],open('google_logging_creds.json','w'))"
+set -x
+cat > td-agent-bit.conf << EOF
+[SERVICE]
+    Daemon       Off
+    Log_File     ~/td-agent-bit.log
+    Log_Level    info
+    Parsers_File parsers.conf
+    Plugins_File plugins.conf
+
+[INPUT]
+    Name tail
+    Path ~/logs/live.log,~/grizzly-auto-run/screenlog.*
+    Path_Key file
+    Key message
+    Refresh_Interval 5
+    Read_from_Head On
+    Skip_Long_Lines On
+    Buffer_Max_Size 1M
+    DB td-grizzly-logs.pos
+
+[FILTER]
+    Name rewrite_tag
+    Match tail.*
+    Rule \$file screenlog.([0-9]+)$ screen\$1.log false
+    Rule \$file ([^\\\\]+)$ \$1 false
+
+[FILTER]
+    Name record_modifier
+    Match *
+    Record host task-${TASK_ID}-run-${RUN_ID}
+    Record pool ${TASKCLUSTER_FUZZING_POOL-unknown}
+    Remove_key file
+
+[OUTPUT]
+    Name stackdriver
+    Match *
+    google_service_credentials ~/google_logging_creds.json
+    resource global
+
+[OUTPUT]
+    Name file
+    Match screen*.log
+    Path ~/logs/
+    Format template
+    Template {time} {message}
+EOF
+fluent-bit -c td-agent-bit.conf &
+
+# Get fuzzmanager configuration from TC
+set +x
+curl --retry 5 -L "$TASKCLUSTER_PROXY_URL/secrets/v1/secret/project/fuzzing/fuzzmanagerconf" | python -c "import json,sys;open('.fuzzmanagerconf','w').write(json.load(sys.stdin)['secret']['key'])"
+set -x
+
+# Update fuzzmanager config for this instance
+mkdir -p signatures
+cat >> .fuzzmanagerconf << EOF
+sigdir = ~/signatures
+tool = bearspray
+EOF
+
+# setup-fuzzmanager-hostname
+name="task-${TASK_ID}-run-${RUN_ID}"
+echo "Using '$name' as hostname." >&2
+echo "clientid = $name" >>.fuzzmanagerconf
+chmod 0600 .fuzzmanagerconf
+
+status "Setup: cloning bearspray"
+
+# Get deployment key from TC
+mkdir -p .ssh
+set +x
+curl --retry 5 -L "$TASKCLUSTER_PROXY_URL/secrets/v1/secret/project/fuzzing/deploy-bearspray" | python -c "import json,sys;open('.ssh/id_ecdsa.bearspray','w',newline='\\n').write(json.load(sys.stdin)['secret']['key'])"
+set -x
+
+cat << EOF >> .ssh/config
+
+Host bearspray
+HostName github.com
+IdentitiesOnly yes
+IdentityFile ~/.ssh/id_ecdsa.bearspray
+EOF
+
+# Checkout bearspray
+git init bearspray
+cd bearspray
+git remote add origin git@bearspray:MozillaSecurity/bearspray.git
+retry git fetch -q --depth 1 --no-tags origin HEAD
+git -c advice.detachedHead=false checkout FETCH_HEAD
+cd ..
+
+status "Setup: installing bearspray"
+retry python -m pip install -U -e bearspray
+
+status "Setup: launching bearspray"
+wait_token="$(rwait create)"
+
+screen -dmLS grizzly /bin/bash
+sleep 5
+screen -S grizzly -X screen rwait run "$wait_token" python -m bearspray "$ADAPTER" --screen
+
+# need to keep the script running
+set +e
+rwait wait "$wait_token"
+exit_code=$?
+echo "returned $exit_code" >&2
+case $exit_code in
+  5)  # grizzly.session.Session.EXIT_FAILURE (reduce no-repro)
+    exit 0
+    ;;
+  *)
+    exit $exit_code
+    ;;
+esac
---
+++ services/grizzly-macos/setup.sh
@@ -0,0 +1,56 @@
+#!/bin/sh
+set -e -x
+
+brew install --force-bottle openssl@1.1 python@3.9
+# shellcheck disable=SC2016
+sed -i '' 's,export PATH=\\",&${HOMEBREW_PREFIX}/opt/python@3.9/libexec/bin:${HOMEBREW_PREFIX}/opt/python@3.9/bin:${HOMEBREW_PREFIX}/opt/python@3.9/Frameworks/Python.framework/Versions/3.9/bin:,' homebrew/Library/Homebrew/cmd/shellenv.sh
+PATH="$HOMEBREW_PREFIX/opt/python@3.9/libexec/bin:$HOMEBREW_PREFIX/opt/python@3.9/bin:$HOMEBREW_PREFIX/opt/python@3.9/Frameworks/Python.framework/Versions/3.9/bin:$PATH"
+
+brew install --force-bottle p7zip zstd
+brew install --force-bottle fluent-bit
+brew install --force-bottle apr-util subversion
+
+brew install --force-bottle node@14
+# shellcheck disable=SC2016
+sed -i '' 's,export PATH=\\",&${HOMEBREW_PREFIX}/opt/node@14/bin:,' homebrew/Library/Homebrew/cmd/shellenv.sh
+PATH="$HOMEBREW_PREFIX/opt/node@14/bin:$PATH"
+curl -qL https://www.npmjs.com/install.sh | npm_install="7.22.0" sh
+
+# configure pip
+mkdir -p pip
+cat << EOF > pip/pip.ini
+[global]
+disable-pip-version-check = true
+no-cache-dir = false
+
+[list]
+format = columns
+
+[install]
+upgrade-strategy = only-if-needed
+progress-bar = off
+EOF
+
+# get minidump_stackwalk
+curl -sSL "https://firefox-ci-tc.services.mozilla.com/api/index/v1/task/gecko.cache.level-1.toolchains.v3.macosx64-minidump-stackwalk.latest/artifacts/public%2Fbuild%2Fminidump_stackwalk.tar.zst" | zstdcat | tar xv --strip 1 -C "$HOMEBREW_PREFIX/bin"
+
+python -V
+node -v
+npm -v
+
+# install utils to match linux ci images
+python -m pip install \
+  psutil \
+  virtualenv
+
+rm -rf homebrew/docs
+
+cp orion/scripts/relocate_homebrew.sh homebrew/bin/
+cp orion/services/grizzly/rwait.py homebrew/bin/rwait
+cp orion/services/grizzly-macos/launch.sh .
+
+cp -r orion/services/fuzzing-decision fuzzing-decision
+python -m pip install ./fuzzing-decision
+
+relocate_homebrew.sh
+tar -jcvf homebrew.tar.bz2 homebrew pip
