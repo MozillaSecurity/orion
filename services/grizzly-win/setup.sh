06cfa3d98d4429a593a43d2b1ad0dc6ce71d5181
Jesse Schwartzentruber <truber@mozilla.com>
2022-02-18 16:10:56 -0500

    [grizzly] Fix mdsw toolchain location

--- services/grizzly-win/setup.sh
+++ services/grizzly-win/setup.sh
@@ -26,7 +26,7 @@ mv "td-agent-bit-${VER}-win64" td-agent-bit
 rm -rf td-agent-bit/include td-agent-bit/bin/fluent-bit.pdb
 
 # get minidump_stackwalk
-curl -sSLO "https://firefox-ci-tc.services.mozilla.com/api/index/v1/task/gecko.cache.level-1.toolchains.v3.win32-minidump-stackwalk.latest/artifacts/public/build/minidump_stackwalk.tar.zst"
+curl -sSLO "https://firefox-ci-tc.services.mozilla.com/api/index/v1/task/gecko.cache.level-3.toolchains.v3.win32-minidump-stackwalk.latest/artifacts/public/build/minidump_stackwalk.tar.zst"
 zstdcat minidump_stackwalk.tar.zst | tar xv
 mv minidump_stackwalk/minidump_stackwalk.exe msys64/usr/bin
 rm -rf minidump_stackwalk minidump_stackwalk.tar.zst
