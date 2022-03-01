2c196fed5f74be4e03123beb6cd7f0bc21c73d53
pyoor <pyoor@users.noreply.github.com>
2022-03-01 15:13:36 -0500

    Use poetry to install bugmon-tc

--- services/bugmon/launch.sh
+++ services/bugmon/launch.sh
@@ -15,6 +15,9 @@ function tc-get-secret () {
   TASKCLUSTER_ROOT_URL="${TASKCLUSTER_PROXY_URL-$TASKCLUSTER_ROOT_URL}" retry taskcluster api secrets get "project/fuzzing/$1"
 }
 
+# shellcheck source=recipes/linux/common.sh
+source ~/.local/bin/common.sh
+
 if [[ -v FORCE_CONFIRM ]]; then
   FORCE_CONFIRM="--force-confirm"
 fi
@@ -23,20 +26,24 @@ export PATH=$PATH:/home/worker/.local/bin
 export ARTIFACT_DEST="/bugmon-artifacts"
 export TC_ARTIFACT_ROOT="project/fuzzing/bugmon"
 
-pip3 install git+https://github.com/MozillaSecurity/bugmon-tc.git
+python3 -m pip install poetry
+git-clone https://github.com/MozillaSecurity/bugmon-tc.git ./bugmon-tc
+cd bugmon-tc
+poetry install
+
 case "$BUG_ACTION" in
   monitor | report)
     BZ_API_KEY="$(tc-get-secret bz-api-key | jshon -e secret -e key -u)"
     export BZ_API_KEY
     export BZ_API_ROOT="https://bugzilla.mozilla.org/rest"
     if [ "$BUG_ACTION" == "monitor" ]; then
-      bugmon-monitor "$ARTIFACT_DEST" $FORCE_CONFIRM
+      poetry run bugmon-monitor "$ARTIFACT_DEST" $FORCE_CONFIRM
     else
-      bugmon-report "$TC_ARTIFACT_ROOT/$PROCESSOR_ARTIFACT"
+      poetry run bugmon-report "$TC_ARTIFACT_ROOT/$PROCESSOR_ARTIFACT"
     fi
     ;;
   process)
-    bugmon-process "$TC_ARTIFACT_ROOT/$MONITOR_ARTIFACT" "$ARTIFACT_DEST/$PROCESSOR_ARTIFACT" $FORCE_CONFIRM --dry-run
+    poetry run bugmon-process "$TC_ARTIFACT_ROOT/$MONITOR_ARTIFACT" "$ARTIFACT_DEST/$PROCESSOR_ARTIFACT" $FORCE_CONFIRM --dry-run
     ;;
   *)
     echo "unknown action: $BUG_ACTION" >&2
