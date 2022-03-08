ef5ccc13beb0582ba8e2ef34c7a3272201bbceca
pyoor <pyoor@users.noreply.github.com>
2022-03-08 11:59:47 -0500

    [gr-css-reports] Explicitly install gr.css@latest

--- services/gr-css-reports/launch.sh
+++ services/gr-css-reports/launch.sh
@@ -30,7 +30,7 @@ git init gr.css.reports
   retry git fetch -q --depth=10 origin main
   git -c advice.detachedHead=false checkout origin/main
   retry npm i --no-progress
-  retry npm i --no-save --no-progress @mozillasecurity/gr.css
+  retry npm i --no-save --no-progress @mozillasecurity/gr.css@latest
   npx gr.css ~/nightly/firefox src/grammar.json &&
   npm test &&
   if ! git diff --quiet src/grammar.json; then
