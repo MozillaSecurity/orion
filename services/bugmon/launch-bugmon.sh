e6f9aa3f2192006402ecda204127592e3f5cfc15
pyoor <pyoor@users.noreply.github.com>
2022-10-27 14:58:47 -0400

    [bugmon] Enable set -x when running bugmon commands

--- services/bugmon/launch-bugmon.sh
+++ services/bugmon/launch-bugmon.sh
@@ -39,33 +39,39 @@ cp -r "$BASE_PY_PATH/pernoscoshared" "$POETRY_PY_PATH"
 # Initialize the grizzly directory to avoid TC errors
 mkdir -p /tmp/grizzly
 
-set +x
 TRACE_ARGS=()
 if [ -v TRACE_ARTIFACT ]; then
   TRACE_ARGS+=("--trace-artifact" "$ARTIFACT_DEST/$TRACE_ARTIFACT")
 fi
 
+set +x
 case "$BUG_ACTION" in
   monitor | report)
     BZ_API_KEY="$(tc-get-secret bz-api-key | jshon -e secret -e key -u)"
     export BZ_API_KEY
     export BZ_API_ROOT="https://bugzilla.mozilla.org/rest"
     if [ "$BUG_ACTION" == "monitor" ]; then
+      set -x
       poetry run bugmon-monitor "$ARTIFACT_DEST" $FORCE_CONFIRM
+      set +x
     else
       PERNOSCO_USER="$(tc-get-secret pernosco-user | jshon -e secret -e key -u)"
       PERNOSCO_GROUP="$(tc-get-secret pernosco-group | jshon -e secret -e key -u)"
       PERNOSCO_USER_SECRET_KEY="$(tc-get-secret pernosco-secret | jshon -e secret -e key -u)"
       export PERNOSCO_USER PERNOSCO_GROUP PERNOSCO_USER_SECRET_KEY
+      set -x
       poetry run bugmon-report "$TC_ARTIFACT_ROOT/$PROCESSOR_ARTIFACT" "${TRACE_ARGS[@]}"
+      set +x
     fi
     ;;
   process)
+    set -x
     poetry run bugmon-process \
       "$TC_ARTIFACT_ROOT/$MONITOR_ARTIFACT" \
       "$ARTIFACT_DEST/$PROCESSOR_ARTIFACT" \
       "${TRACE_ARGS[@]}" \
       $FORCE_CONFIRM
+    set +x
     ;;
   *)
     echo "unknown action: $BUG_ACTION" >&2
