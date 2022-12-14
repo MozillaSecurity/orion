1af4e7c8ad0d73e1bd0cbb60a4dfe720abd5cfaf
pyoor <pyoor@users.noreply.github.com>
2022-12-14 10:47:05 -0500

    [gr-css-reports] Use prefs when running gr.css

--- services/gr-css-reports/launch.sh
+++ services/gr-css-reports/launch.sh
@@ -20,6 +20,9 @@ export CI=1
 export EMAIL=nobody@community-tc.services.mozilla.com
 export {GIT_AUTHOR_NAME,GIT_COMMITTER_NAME}="Taskcluster Automation"
 
+# Install prefpicker
+retry python3 -m pip install git+https://github.com/MozillaSecurity/prefpicker.git
+
 # Fetch build
 fuzzfetch -a --fuzzing -n nightly
 
@@ -27,14 +30,15 @@ git init gr.css.reports
 (
   cd gr.css.reports
   git remote add origin "${GIT_REPO-git@gr-css-reports:MozillaSecurity/gr.css.reports}"
-  retry git fetch -q --depth=10 origin main
-  git -c advice.detachedHead=false checkout origin/main
+  retry git fetch -q --depth=10 origin v2
+  git -c advice.detachedHead=false checkout origin/v2
   retry npm i --no-package-lock --no-progress --no-save
-  retry npm i --no-save --no-progress @mozillasecurity/gr.css@latest
-  npx gr.css ~/nightly/firefox src/grammar.json &&
+  retry npm i --no-progress @mozillasecurity/gr.css@next
+  python3 -m prefpicker browser-fuzzing.yml prefs.js
+  npx gr.css ~/nightly/firefox src/grammar.json -p prefs.js &&
   npm test &&
   if ! git diff --quiet src/grammar.json; then
     git commit -m "chore(grammar): update grammar" src/grammar.json
-    retry git push origin HEAD:main
+    retry git push origin HEAD:v2
   fi
 )> /live.log 2>&1
