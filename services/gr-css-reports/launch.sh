9c328b9b05ec922eda4d56288c011034c261d728
pyoor <pyoor@users.noreply.github.com>
2022-12-27 10:43:54 -0500

    [gr-css-reports] Ignore lockfile when installing gr.css

--- services/gr-css-reports/launch.sh
+++ services/gr-css-reports/launch.sh
@@ -32,6 +32,8 @@ git init gr.css.reports
   git remote add origin "${GIT_REPO-git@gr-css-reports:MozillaSecurity/gr.css.reports}"
   retry git fetch -q --depth=10 origin v2
   git -c advice.detachedHead=false checkout origin/v2
+  # Ignore the lockfile when installing both to ensure we have the latest
+  # version of gr.css and gr.css.generator
   retry npm i --no-package-lock --no-progress --no-save
   retry npm i --no-package-lock --no-progress @mozillasecurity/gr.css@next
   python3 -m prefpicker browser-fuzzing.yml prefs.js
