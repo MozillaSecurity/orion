e93e61c553fbc12ed8eb437835c403034b43f10c
pyoor <pyoor@users.noreply.github.com>
2023-02-06 14:15:22 -0500

    [coverage-revision] Use CI coverage revision when possible

--- services/coverage-revision/launch.sh
+++ services/coverage-revision/launch.sh
@@ -1,18 +1,22 @@
-#!/bin/sh
-set -e -x
-# store and kill self in pipeline
-# this emulates bash -o pipefail
-self=$$
+#!/bin/bash
+set -e
+set -x
+set -o pipefail
+
+# shellcheck source=recipes/linux/common.sh
+source "$HOME/.local/bin/common.sh"
 
 DST="${DST-/coverage-revision.txt}"
 
-{ fuzzfetch --coverage --fuzzing -a --dry-run 2>&1 || kill $self; } | { tee /dev/stderr || kill $self; } | sed -n 's/.*> Changeset: \(.*\)/\1/p' > "$DST"
-
-# Validate that we got a proper revision
+CI_LATEST=$(retry-curl --silent https://api.coverage.moz.tools/v2/history | jq -r '.[0].changeset')
+if [ -n "$CI_LATEST" ] && fuzzfetch --build "$CI_LATEST" --coverage --fuzzing -a --dry-run 2>&1; then
+  echo "$CI_LATEST" > "$DST"
+else
+  fuzzfetch --coverage --fuzzing -a --dry-run 2>&1 | tee /dev/stderr | sed -n 's/.*> Changeset: \(.*\)/\1/p' > "$DST"
+fi
 
 ## Check that file isn't empty
 [ -s "$DST" ]
 
 ## Check that the revision exists on mozilla-central
-retry_curl () { curl -sSL --connect-timeout 25 --fail --retry 5 "$@"; }
-retry_curl --head "https://hg.mozilla.org/mozilla-central/rev/$(cat "$DST")" > /dev/null
+retry-curl --head "https://hg.mozilla.org/mozilla-central/rev/$(cat "$DST")" > /dev/null
