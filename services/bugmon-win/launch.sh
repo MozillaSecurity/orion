cde123a05ce6b1e642b07a3535c1d63377568fc1
pyoor <pyoor@users.noreply.github.com>
2023-12-04 09:28:09 -0500

    [bugmon-win] Initial commit (#360)
    * [bugmon-win] Initial commit
    
    * Remove unused code
    
    * Normalize indents
    
    * Copy launch script
    
    * Build msys2.tar.bz2

---
+++ services/bugmon-win/launch.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+set -e -x
+PATH="$PWD/msys64/opt/node:$PATH"
+
+retry () {
+  i=0
+  while [ "$i" -lt 9 ]
+  do
+    "$@" && return
+    sleep 30
+    i="$((i+1))"
+  done
+  "$@"
+}
+retry_curl () { curl -sSL --connect-timeout 25 --fail --retry 5 -w "%{stderr}[downloaded %{url_effective}]\n" "$@"; }
+
+powershell -ExecutionPolicy Bypass -NoProfile -Command "Set-MpPreference -DisableScriptScanning \$true" || true
+powershell -ExecutionPolicy Bypass -NoProfile -Command "Set-MpPreference -DisableRealtimeMonitoring \$true" || true
+
+retry python -m pip install git+https://github.com/MozillaSecurity/bugmon.git
+
+ARTIFACT_DEST="$USERPROFILE/bugmon-artifacts"
+TC_ARTIFACT_ROOT="$USERPROFILE/project/fuzzing/bugmon"
+
+CONFIRM_ARGS=""
+if [ -n "$FORCE_CONFIRM" ]; then
+  CONFIRM_ARGS="--force-confirm"
+fi
+
+bugmon-process \
+  "$TC_ARTIFACT_ROOT/$MONITOR_ARTIFACT" \
+  "$ARTIFACT_DEST/$PROCESSOR_ARTIFACT" \
+  $CONFIRM_ARGS >"$ARTIFACT_DEST/live.log" 2>&1
