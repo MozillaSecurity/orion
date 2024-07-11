41a75f3b28bcf0deea8ffa5658bf78b24599543e
Tyson Smith <tyson.w.smith@gmail.com>
2024-07-11 10:07:51 -0700

    [site-scout] Install ripgrep

--- services/site-scout/setup.sh
+++ services/site-scout/setup.sh
@@ -7,11 +7,22 @@ set -e
 set -x
 set -o pipefail
 
+# shellcheck source=recipes/linux/common.sh
+source "${0%/*}/common.sh"
+
+#### Bootstrap Packages
+
+sys-update
+
 #### Install recipes
 /src/recipes/grcov.sh
+
+sys-embed ripgrep
 
 # Cleanup grizzly scripts
 rm /home/worker/launch-grizzly*
 
 #### Fix ownership
 chown -R worker:worker /home/worker
+
+"${0%/*}/cleanup.sh"
