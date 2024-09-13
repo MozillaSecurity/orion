eede8943d08b98463b535c6afe00d37eeddef577
Akmat Suleimanov <asuleimanov@mozilla.com>
2024-09-13 15:24:09 -0400

    [orion-builder] Build arm64 image with regular amd64

--- services/orion-builder/src/orion_builder/build.py
+++ services/orion-builder/src/orion_builder/build.py
@@ -104,12 +104,26 @@ class BuildArgs(CommonArgs):
 def main(argv: Optional[List[str]] = None) -> None:
     """Build entrypoint. Does not return."""
     args = BuildArgs.parse_args(argv)
-    configure_logging(level=args.log_level)
-    target = Target(args)
-    if args.load_deps:
+    # TODO: remove and set arm/amd args properly (if more than just a tag)
+    args_arm64 = BuildArgs.parse_args(argv)
+    args_amd64 = BuildArgs.parse_args(argv)
+    args_arm64.tag = [args_arm64.git_revision, "latest-arm64"]
+    args_amd64.tag = [args_amd64.git_revision, "latest-amd64"]
+
+    configure_logging(level=args.log_level)  # TODO: split?
+    # target = Target(args)
+    target_arm64 = Target(args_arm64)
+    target_amd64 = Target(args_amd64)
+
+    if args.load_deps:  # TODO: split?
         stage_deps(args)
     try:
-        build_image(target, args)
+        # build_image(target, args)  # TODO: add args.tag = "latest-arm64" and amd64
+        build_image(target_arm64, args_amd64)
+        build_image(target_amd64, args_arm64)
+        # //TODO: add the 2 images to manifest
     finally:
-        rmtree(target.dir)
+        rmtree(target_arm64.dir)
+        rmtree(target_amd64.dir)
+
     sys.exit(0)
