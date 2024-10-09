29cbea7c4f5ba5e20a2b45f63982a24aa6b79c8b
Akmat Suleimanov <asuleimanov@mozilla.com>
2024-10-09 15:17:56 -0400

    Add combine.py task to save multiarch builds

--- services/orion-builder/src/orion_builder/combine.py
+++ services/orion-builder/src/orion_builder/combine.py
@@ -1,29 +1,27 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
-"""CLI for Orion builder/build script"""
+"""CLI for Orion builder/combine script"""
 
 
 import argparse
-import logging
 import sys
 from os import getenv
 from pathlib import Path
-from platform import machine
 from shutil import rmtree
+from tempfile import mkdtemp
 from typing import List, Optional
 
-from taskboot.build import build_image
-from taskboot.target import Target
+import taskcluster
+from taskboot.config import Configuration
+from taskboot.docker import Podman
+from taskboot.utils import download_artifact, load_artifacts
 
-from .cli import CommonArgs, configure_logging
-from .stage_deps import stage_deps
-
-logger = logging.getLogger(__name__)
+from .cli import CommonArgs
 
 
-class BuildArgs(CommonArgs):
-    """CLI arguments for Orion builder"""
+class CombineArgs(CommonArgs):
+    """CLI arguments for Orion combiner"""
 
     def __init__(self) -> None:
         super().__init__()
@@ -37,24 +35,19 @@ class BuildArgs(CommonArgs):
         self.parser.add_argument(
             "--build-tool",
             default=getenv("BUILD_TOOL"),
-            help="Tool to use for building (img/dind) (default: BUILD_TOOL)",
-            choices={"img", "dind"},
+            help="Tool for combining builds into multiarch image (default: BUILD_TOOL)",
+            choices={"podman", "docker"},
         )
         self.parser.add_argument(
-            "--dockerfile",
-            default=getenv("DOCKERFILE"),
-            help="Path to the Dockerfile (default: Dockerfile)",
-        )
-        self.parser.add_argument(
-            "--build-arg",
+            "--archs",
             action="append",
-            default=[],
-            help="Docker build args",
+            default=getenv("ARCHS", ["amd64"]),
+            help="Architectures to be included in the multiarch image",
         )
         self.parser.add_argument(
             "--image",
             default=getenv("IMAGE_NAME"),
-            help="Docker image name (without registry, default: IMAGE_NAME)",
+            help="Docker image name (without repository, default: IMAGE_NAME)",
         )
         self.parser.add_argument(
             "--load-deps",
@@ -90,8 +83,8 @@ class BuildArgs(CommonArgs):
         if args.build_tool is None:
             self.parser.error("--build-tool (or BUILD_TOOL) is required!")
 
-        if args.dockerfile is None:
-            self.parser.error("--dockerfile (or DOCKERFILE) is required!")
+        if args.archs is None:
+            self.parser.error("--archs is required!")
 
         if args.image is None:
             self.parser.error("--image (or IMAGE_NAME) is required!")
@@ -103,17 +96,49 @@ class BuildArgs(CommonArgs):
 
 
 def main(argv: Optional[List[str]] = None) -> None:
-    """Build entrypoint. Does not return."""
-    args = BuildArgs.parse_args(argv)
-    arch = {"x86_64": "amd64", "aarch64": "arm64"}.get(machine(), machine())
-    # TODO: assert arch = $ARCH passed in scheduler.py:create_tasks()?
-    args.tag = [args.git_revision, f"latest-{arch}"]
-    configure_logging(level=args.log_level)
-    target = Target(args)
-    if args.load_deps:
-        stage_deps(args)
+    """Combine entrypoint. Does not return."""
+
+    args = CombineArgs.parse_args(argv)
+    service_name = args.service_name
+    archs = args.archs
+    config = Configuration(argparse.Namespace(secret=None, config=None))
+    queue = taskcluster.Queue(config.get_taskcluster_options())
+
+    # 1. Load all archs images into podman
+    tool = Podman()
+    # retrieve image archives from dependency tasks to /images
+    image_path = Path(mkdtemp(prefix="image-deps-"))
     try:
-        build_image(target, args)
+        config = Configuration(argparse.Namespace(secret=None, config=None))
+        queue = taskcluster.Queue(config.get_taskcluster_options())
+
+        artifacts_ids = load_artifacts(args.task_id, queue, "public/**.tar.zst")
+
+        for task_id, artifact_name in artifacts_ids:
+            img = download_artifact(queue, task_id, artifact_name, image_path)
+            # load images into the podman image store
+            load_result = tool.run(
+                [
+                    "load",
+                    "--input",
+                    str(img),
+                ],
+                text=True,  # TODO: check
+            )
+            print(load_result)
+            img.unlink()
+        # TODO: some checks that loaded artifacts = images for specified archs
+
+        # 2. Save loaded images into a single multiarch .tar
+        save_result = tool.run(
+            ["save", "--multi-image-archive"]
+            + [
+                f"{args.registry}/mozillasecurity/{service_name}:latest-{arch}"
+                for arch in archs
+            ]
+            + ["--output", f"{service_name}.tar"]  # TODO: check this artifact on TC
+        )
+        print(save_result)
     finally:
-        rmtree(target.dir)
+        rmtree(image_path)
     sys.exit(0)
