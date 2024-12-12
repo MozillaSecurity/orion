220ce88ac1ee8117368a3005522e8374b89be992
pyoor <pyoor@users.noreply.github.com>
2024-12-12 10:59:30 -0500

    [recipes] Remove duplicate definitions of get-tc-secreet

--- services/domino-web-tests/launch.sh
+++ services/domino-web-tests/launch.sh
@@ -3,23 +3,14 @@
 set -e
 set -o pipefail
 
-function retry () {
-  for _ in {1..9}; do
-    "$@" && return
-    sleep 30
-  done
-  "$@"
-}
+# shellcheck source=recipes/linux/common.sh
+source "${0%/*}/common.sh"
 
-function get-secret () {
-  TASKCLUSTER_ROOT_URL="${TASKCLUSTER_PROXY_URL-$TASKCLUSTER_ROOT_URL}" retry taskcluster api secrets get "$1"
-}
-
-get-secret project/fuzzing/deploy-domino-web-tests | jshon -e secret -e key -u >.ssh/id_ecdsa.domino_web_tests
+get-tc-secret project/fuzzing/deploy-domino-web-tests >.ssh/id_ecdsa.domino_web_tests
 ln -s id_ecdsa.domino_web_tests .ssh/id_ecdsa
-get-secret project/fuzzing/deploy-domino | jshon -e secret -e key -u >.ssh/id_rsa.domino
-get-secret project/fuzzing/deploy-gridl | jshon -e secret -e key -u >.ssh/id_rsa.gridl
-get-secret project/fuzzing/deploy-octo-private | jshon -e secret -e key -u >.ssh/id_rsa.octo
+get-tc-secret project/fuzzing/deploy-domino >.ssh/id_rsa.domino
+get-tc-secret project/fuzzing/deploy-gridl >.ssh/id_rsa.gridl
+get-tc-secret project/fuzzing/deploy-octo-private >.ssh/id_rsa.octo
 set -x
 chmod 0400 .ssh/id_*
 
