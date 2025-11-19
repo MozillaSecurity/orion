8f170d853c7244d2c99542808229125a8aebc08e
pyoor <pyoor@users.noreply.github.com>
2025-11-19 12:18:40 -0500

    feat(grizzly): use llvm-cov for grcov

--- services/grizzly/setup.sh
+++ services/grizzly/setup.sh
@@ -24,8 +24,6 @@ cd "${0%/*}"
 ./fluentbit.sh
 EDIT=1 SRCDIR=/src/fuzzing-tc ./fuzzing_tc.sh
 EDIT=1 DESTDIR=/src ./fuzzmanager.sh
-./gcov-9.sh
-./grcov.sh
 ./nodejs.sh
 ./radamsa.sh
 ./rr.sh
@@ -146,6 +144,14 @@ export PS1='🐳  \[\033[1;36m\]\h \[\033[1;34m\]\W\[\033[0;35m\] \[\033[1;36m\]
 EOF
 
 mkdir -p /home/worker/.local/bin
+
+# Install grcov
+./grcov.sh
+./llvm-cov.sh
+
+# Symlink llvm-cov to gcov to ensure it's used by grcov
+ln -s "$(which llvm-cov)" /usr/local/bin/gcov
+strip --strip-unneeded /usr/local/bin/llvm-cov
 
 # Add `cleanup.sh` to let images perform standard cleanup operations.
 cp "${0%/*}/cleanup.sh" /home/worker/.local/bin/cleanup.sh
